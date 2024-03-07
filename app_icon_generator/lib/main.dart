2459bf85cde76761f67809f68f7f09716a8a8c9f
Dhaval <dhavalpithadiya015@gmail.com>
2024-03-07 16:45:49 +0530

    - changes

--- app_icon_generator/lib/main.dart
+++ app_icon_generator/lib/main.dart
@@ -4,10 +4,7 @@ import 'dart:io';
 import 'src/helper_function.dart';
 
 void main(List<String> args) {
-  print(args);
-  final notificationIcon = args[1] == 'true' ? true : false;
-  print("===============> ${notificationIcon}");
-  _init(path: args[0], notificationIcon: notificationIcon);
+  _init(path: args[0], notificationIcon: bool.parse(args[1]));
 }
 
 Future<void> _init({required String path, required bool notificationIcon}) async {
@@ -15,11 +12,7 @@ Future<void> _init({required String path, required bool notificationIcon}) async
   if (notificationIcon) notificationIconPath = "$path/required_files/notification_icon.png";
   final iconRecord = getImageDimension(appIconPath: "$path/required_files/app_icon.png", notificationIconPath: notificationIconPath);
   if (iconRecord == null) return;
-
   resizeImage(appIcon: iconRecord.appIcon!, path: path, notificationIcon: iconRecord.notificationIcon);
-
-  // if (decodedImage == null) return;
-  // resizeImage(decodedImage: decodedImage, path: path);
 }
 
 // if (decodedImage.height == 1024 && decodedImage.width == 1024) {
--- app_icon_generator/lib/src/helper_function.dart
+++ app_icon_generator/lib/src/helper_function.dart
@@ -2,34 +2,10 @@ import 'dart:io';
 import 'package:image/image.dart' as image;
 import 'constant.dart';
 
-// ({image.Image appIcon, image.Image? notificationIcon})? getImageDimension({
-//   required String imagePath,
-//   String? notificationIconPath,
-// }) {
-//   ({image.Image icon, image.Image? notification}) binder;
-//   try {
-//     final appIcon = image.decodeImage(File(imagePath).readAsBytesSync());
-//     if (appIcon == null) return null;
-//     if (notificationIconPath != null) {
-//       final notificationImage = image.decodeImage(File(notificationIconPath).readAsBytesSync());
-//       if (notificationImage == null) return null;
-//       binder = (icon: appIcon, notification: notificationImage);
-//     }
-//
-//     if (notificationIconPath != null) {
-//       final notificationImage = image.decodeImage(File(notificationIconPath).readAsBytesSync());
-//     }
-//
-//     if (decodedImage == null) return null;
-//
-//     return decodedImage;
-//   } catch (e) {
-//     print("error => $e");
-//   }
-//   return null;
-// }
-
-({image.Image? appIcon, image.Image? notificationIcon})? getImageDimension({required String appIconPath, String? notificationIconPath}) {
+({image.Image? appIcon, image.Image? notificationIcon})? getImageDimension({
+  required String appIconPath,
+  String? notificationIconPath,
+}) {
   if (notificationIconPath != null) {
     final appIcon = image.decodeImage(File(appIconPath).readAsBytesSync());
     final notificationIcon = image.decodeImage(File(notificationIconPath).readAsBytesSync());
@@ -41,25 +17,19 @@ import 'constant.dart';
 }
 
 void resizeImage({required image.Image appIcon, required String path, image.Image? notificationIcon}) async {
-  final resizedAndroidImageList = <File>[];
-  final resizedIosImageList = <File>[];
-  final resizedNotificationImageList = <File>[];
-
   try {
     for (int i = 0; i < androidTemplateList.length; i++) {
       final androidDirectory = await Directory("$androidFolderPath/${androidTemplateList[i].directoryName}").create(recursive: true);
       final resizeImage = image.copyResize(appIcon, width: androidTemplateList[i].size, height: androidTemplateList[i].size, maintainAspect: true);
       final unit8List = image.encodePng(resizeImage);
-      final file = await File("${androidDirectory.path}/ic_launcher.png").writeAsBytes(unit8List);
-      //resizedAndroidImageList.add(file);
+      await File("${androidDirectory.path}/ic_launcher.png").writeAsBytes(unit8List);
     }
 
     final iosDirectory = await Directory(iosFolderPath).create(recursive: true);
     for (int i = 0; i < iosTemplateList.length; i++) {
       final resizeImage = image.copyResize(appIcon, width: iosTemplateList[i].size, height: iosTemplateList[i].size, maintainAspect: true);
       final unit8List = image.encodePng(resizeImage);
-      final file = await File("${iosDirectory.path}/${iosTemplateList[i].fileName}").writeAsBytes(unit8List);
-      //resizedIosImageList.add(file);
+      await File("${iosDirectory.path}/${iosTemplateList[i].fileName}").writeAsBytes(unit8List);
     }
 
     final jsonFilePath = "$path/app_icon_generator/lib/src/contents.json";
@@ -76,8 +46,7 @@ void resizeImage({required image.Image appIcon, required String path, image.Imag
           maintainAspect: true,
         );
         final unit8List = image.encodePng(resizedIcon);
-        final file = await File("${androidDirectory.path}/ic_notification.png").writeAsBytes(unit8List);
-        //resizedNotificationImageList.add(file);
+        await File("${androidDirectory.path}/ic_notification.png").writeAsBytes(unit8List);
       }
     }
 
@@ -86,3 +55,4 @@ void resizeImage({required image.Image appIcon, required String path, image.Imag
     print("error => $e");
   }
 }
+
